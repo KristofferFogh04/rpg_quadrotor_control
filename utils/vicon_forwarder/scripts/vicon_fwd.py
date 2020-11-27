f5b5aea746e5b626e90817235dfa56ec6753aad2
Kristoffer Fogh Andersen <Kristoffer_fogh@hotmail.com>
2020-11-27 11:47:27 +0100

    Removed px4keepalive

--- utils/vicon_forwarder/scripts/vicon_fwd.py
+++ utils/vicon_forwarder/scripts/vicon_fwd.py
@@ -2,7 +2,7 @@
 
 import sys
 import rospy
-from geometry_msgs.msg import PoseStamped, Transform, Vector3, Quaternion
+from geometry_msgs.msg import PoseStamped, TransformStamped, Vector3, Quaternion
 from nav_msgs.msg import Odometry
 
 class ViconAdapter:
@@ -11,20 +11,20 @@ class ViconAdapter:
         sub_name = str(argv[1])
         seg_name = str(argv[2])
         rospy.init_node("vicon_forwarder", anonymous=True)
-        self.sub = rospy.Subscriber("vicon/" + sub_name + "/" + seg_name, Transform, self.vicon_callback)
+        self.sub = rospy.Subscriber("vicon/" + sub_name + "/" + seg_name, TransformStamped, self.vicon_callback)
         self.pub = rospy.Publisher("/mavros/vision_pose/pose", PoseStamped, queue_size=10)
         self.pub_msg = PoseStamped()
-        self.pub_msg.header.frame_id = 'odom'
+        self.pub_msg.header.frame_id = 'map'
         self.seq = 0
 
     def vicon_callback(self, data):
         self.pub_msg.header.seq = self.seq
         self.seq += 1
         self.pub_msg.header.stamp = rospy.get_rostime()
-        self.pub_msg.pose.position.x = data.translation.x
-        self.pub_msg.pose.position.y = data.translation.y
-        self.pub_msg.pose.position.z = data.translation.z
-        self.pub_msg.pose.orientation = data.rotation
+        self.pub_msg.pose.position.x = data.transform.translation.x
+        self.pub_msg.pose.position.y = data.transform.translation.y
+        self.pub_msg.pose.position.z = data.transform.translation.z
+        self.pub_msg.pose.orientation = data.transform.rotation
         self.pub.publish(self.pub_msg)
 
 def main(argv):
