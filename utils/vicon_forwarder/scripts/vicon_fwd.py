64da3a50d5bc99b18c992434c3de63637b47c7c2
Kristoffer Fogh Andersen <kristoffer_fogh@hotmail.com>
2020-12-07 10:37:59 +0100

    Now we also relay the vicon ground trhuith as odometry

--- utils/vicon_forwarder/scripts/vicon_fwd.py
+++ utils/vicon_forwarder/scripts/vicon_fwd.py
@@ -13,19 +13,28 @@ class ViconAdapter:
         rospy.init_node("vicon_forwarder", anonymous=True)
         self.sub = rospy.Subscriber("vicon/" + sub_name + "/" + seg_name, TransformStamped, self.vicon_callback)
         self.pub = rospy.Publisher("/mavros/vision_pose/pose", PoseStamped, queue_size=10)
+        self.pub_plot = rospy.Publisher("autopilot/vicon_reference", Odometry, queue_size=10)
         self.pub_msg = PoseStamped()
         self.pub_msg.header.frame_id = 'map'
+        self.plot_msg = Odometry()
+        self.plot_msg.header.frame_id = 'odom'
         self.seq = 0
 
     def vicon_callback(self, data):
         self.pub_msg.header.seq = self.seq
-        self.seq += 1
         self.pub_msg.header.stamp = rospy.get_rostime()
         self.pub_msg.pose.position.x = data.transform.translation.x
         self.pub_msg.pose.position.y = data.transform.translation.y
         self.pub_msg.pose.position.z = data.transform.translation.z
         self.pub_msg.pose.orientation = data.transform.rotation
         self.pub.publish(self.pub_msg)
+
+        self.plot_msg.header.seq = self.seq
+        self.plot_msg.header.stamp = rospy.get_rostime()
+        self.plot_msg.pose.pose = data.pose
+        self.pub_plot.publish(self.plot_msg)
+
+        self.seq += 1
 
 def main(argv):
     va = ViconAdapter(argv)
